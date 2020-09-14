d06679446f655462c3053974f3adf30bf8e8198a
taesunpark <tovbskvb@daum.net>
2020-09-14 16:37:23 +0900

    최종

--- app/src/main/java/com/example/subscriptionforme/home/FragmentHome.java
+++ app/src/main/java/com/example/subscriptionforme/home/FragmentHome.java
@@ -91,7 +91,8 @@ public class FragmentHome extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        imageButton = getActivity().findViewById(R.id.setting);
+        imageButton = view.findViewById(R.id.setting);
+
 
         // 설정 페이지 엑티비티 가는 버튼
         imageButton.setOnClickListener(new View.OnClickListener(){
